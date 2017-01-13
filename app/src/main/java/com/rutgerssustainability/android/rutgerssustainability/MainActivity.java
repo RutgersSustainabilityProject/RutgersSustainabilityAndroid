3e08d730c9355533a7039f158f1b26d44ccb72db
shreyashirday@gmail.com <shreyashirday@gmail.com>
2017-01-13 15:34:11 -0500

    added picture taking functionality; to be tested

--- app/src/main/java/com/rutgerssustainability/android/rutgerssustainability/MainActivity.java
+++ app/src/main/java/com/rutgerssustainability/android/rutgerssustainability/MainActivity.java
@@ -1,13 +1,32 @@
 package com.rutgerssustainability.android.rutgerssustainability;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button takePicBtn;
     private Button viewPicsBtn;
+
+    private static final String TAG = "MainActivity";
+    private final static int REQUEST_TAKE_PHOTO = 1;
+
+    String mCurrentPhotoPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +34,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         takePicBtn = (Button)findViewById(R.id.take_pic_btn);
         viewPicsBtn = (Button)findViewById(R.id.view_pics_btn);
+
+        takePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executeTakePictureIntent();
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            //TODO: make intent go to after photo activity
+            Intent afterPhoto = new Intent();
+            extras.putString("path",mCurrentPhotoPath);
+            afterPhoto.putExtras(extras);
+            startActivity(afterPhoto);
+        }
+
+    }
+
+    private void executeTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile(MainActivity.this);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(TAG,ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    private File createImageFile(Context context) throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
+
 }
